c77033fd9c684e58ab1aa01e75c7d9a3e885e5ec
Tyler Lawson <tylerjlawson2@gmail.com>
2021-02-27 13:07:01 -0500

    setup calls to rapid api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,30 @@
 import express from 'express';
+import { default as axios } from 'axios';
+import { token } from '../../env.json';
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (_req, res) {
-  res.render('index', { title: 'World' });
+  // TODO get next three teams given an id
+  var options = {
+    method: 'GET',
+    url: 'https://basketball-data.p.rapidapi.com/match/list',
+    params: { date: '29/01/2021' },
+    headers: {
+      'x-rapidapi-key': token,
+      'x-rapidapi-host': 'basketball-data.p.rapidapi.com'
+    }
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 export default router;
