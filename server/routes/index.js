00b2249e884a3b61e47d19a244f2cdf4ff249978
Tyler Lawson <tylerjlawson2@gmail.com>
2021-02-27 22:02:31 -0500

    ascii for html and also standardized types

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,30 @@
 import express from 'express';
-import { emojify } from 'node-emoji';
 import { table } from 'table';
 import { mockBBall } from '../mock';
-// import { default as axios } from 'axios';
+import Convert from 'ansi-to-html';
+// import axios from 'axios';
 
-var router = express.Router();
+/**
+ * data for a team
+ * @typedef {Object} Team
+ * @param {string} abbr - abbreviation for the team
+ * @param {number} score - points for team
+ */
+
+/**
+ * game object storing data for one scheduled game
+ * @typedef {Object} Game
+ * @param {Team} home - home team
+ * @param {Team} away - away team
+ * @param {string} datetime - date and time of game
+ */
+
+/**
+ * Sport with games
+ * @typedef {Object} Sport
+ * @property {string} name - name of the sport
+ * @property {Game[]} hasPower - Indicates whether the Power component is present.
+ */
 
 // helper to get the bball data from the api
 // const getBBall = (teamId) => {
@@ -18,45 +38,90 @@ var router = express.Router();
 //   });
 // };
 
+/**
+ * helper to remove leading zero from string
+ * e.g. 02 -> 2
+ * @param {string} str
+ */
 const removeLeadingZero = (str) => (str[0] === '0' ? str[1] : str);
 
-const formatGame = (game) => {
+/**
+ * helper to normalize data from balldontlie api
+ * @param {Object} game - game from balldontlie to standardize
+ * @returns {Game}
+ */
+const translateBallDontLie = (game) => {
   // parsed iso string
   const [date] = game.date.split('T');
   const [year, month, day] = date.split('-').map(removeLeadingZero);
   const gameDateString = `${month}/${day}/${year} ${game.status}`;
-
-  return [
-    `${game.home_team.abbreviation} vs ${game.visitor_team.abbreviation}`,
-    gameDateString
-  ].join('\n');
+  return {
+    datetime: gameDateString,
+    home: {
+      abbr: game.home_team.abbreviation,
+      score: game.home_team_score
+    },
+    away: {
+      abbr: game.visitor_team.abbreviation,
+      score: game.visitor_team_score
+    }
+  };
 };
 
+// lookup table to hold emoji for each sport
+const emojiMap = {
+  hockey: '🏒',
+  basketball: '🏀',
+  baseball: '⚾',
+  football: '🏈'
+};
+
+/**
+ * helper function to format a game object for the table cells
+ * @param {Game} game - game to format as a string
+ */
+const formatGame = (game) => {
+  return [`${game.home.abbr} vs ${game.away.abbr}`, game.datetime].join('\n');
+};
+
+var router = express.Router();
 router.get('/', async function (req, res) {
   // Testing for presence of curl in user agent
   // goal of this is to test if the request is coming from a terminal vs a browser
   // Want to return ascii text for curl requests and html for the browser
+  // TODO make team dynamic, 2 is for the celtics
+  // const teamId = 2;
+
+  let nextThreeBBall;
+  // for real api call
+  // const bballResponse = await getBBall(teamId);
+  // const bballData = bballResponse.data;
+
+  // nextThreeBBall = bballData.data.slice(0, 3);
+
+  // load three from mock
+  nextThreeBBall = mockBBall.data.slice(0, 3).map(translateBallDontLie);
+
+  const dataForTable = [
+    {
+      name: 'basketball',
+      games: nextThreeBBall
+    }
+  ];
+
+  const gamesTable = table(
+    dataForTable.map((sport) => [
+      `${emojiMap[sport.name]} ${sport.name}`,
+      ...sport.games.map(formatGame)
+    ])
+  );
+
   if (req.headers['user-agent'].includes('curl')) {
-    // TODO make team dynamic, 2 is for the celtics
-    // const teamId = 2;
-
-    let nextThreeBBall;
-    // for real api call
-    // const bballResponse = await getBBall(teamId);
-    // const bballData = bballResponse.data;
-
-    // nextThreeBBall = bballData.data.slice(0, 3);
-
-    // load three from mock
-    nextThreeBBall = mockBBall.data.slice(0, 3);
-
-    const gamesTable = table([
-      [emojify(':basketball: Basketball'), ...nextThreeBBall.map(formatGame)]
-    ]);
-
     res.send(gamesTable);
   } else {
-    res.render('index', { title: 'the end' });
+    const convert = new Convert();
+    const htmlTable = convert.toHtml(gamesTable);
+    res.render('index', { table: htmlTable });
   }
 });
 
