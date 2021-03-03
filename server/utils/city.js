a90aaace068a7ee3b94f3e7530d327b912f4a37b
Tyler Lawson <tylerjlawson2@gmail.com>
2021-03-03 00:46:18 -0500

    logs and stuff for prod

--- server/utils/city.js
+++ server/utils/city.js
@@ -7,6 +7,7 @@ export const getCity = async req => {
     const { data } = await axios.get('http://bot.whatismyipaddress.com');
     ip = data;
   }
+  console.log('IP ADDRESS:', ip);
   const geoResponse = await axios
     .get('https://tools.keycdn.com/geo.json?host=' + ip, {
       headers: { 'user-agent': 'keycdn-tools:https://www.sprt.dev' }
@@ -14,14 +15,16 @@ export const getCity = async req => {
     .catch(err => console.log('Error getting geolocation from ip', err));
 
   const geo = geoResponse.data.data.geo;
-
+  console.log('GEO result', geo);
   // TODO dynamic access to city
   // get closest city based on location for each sport
   // keep city in a standard way that will work with espn and hopefully anything else
   // probably keep long name of city, and sport name abbreviation
 
   return {
-    name: `${geo.city}, ${geo.region_name}, ${geo.country_name} (Schedule hardcoded to Boston for now)`,
+    name:
+      [geo.city, geo.region_name, geo.country_name].join(', ') +
+      '(Schedule hardcoded to Boston for now)',
     sports: {
       baseball: 'bos',
       basketball: 'bos',
