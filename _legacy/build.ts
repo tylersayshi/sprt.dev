23b6714571f5f0cc648ef05c7b0485791c82b1e3
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-11-03 13:16:33 -0800

    remove old code

--- _legacy/build.ts
+++
@@ -1,7 +0,0 @@
-await Bun.build({
-  entrypoints: ['./src/index.ts'],
-  outdir: './dist',
-  target: 'bun',
-  format: 'esm',
-  minify: true
-});
--- _legacy/utils/fetch-data.ts
+++
@@ -1,3 +0,0 @@
-/** optimistic helper for node fetch */
-export const fetchData = <T>(...params: Parameters<typeof fetch>): Promise<T> =>
-  fetch(...params).then(res => res.json()) as Promise<T>;
