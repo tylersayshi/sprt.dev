7353abfe0af50ff89ecb388249223f3edb656194
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-11-03 22:55:15 -0800

    simplify deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 GOOS=linux GOARCH=amd64 go build -o myapp
 sftp -i ~/.ssh/droplet root@sprt.dev <<< 'put myapp sprt-dev'
 
-echo "sudo systemctl restart sprt-dev.service && echo 'Done!'" | ssh -i ~/.ssh/droplet root@sprt.dev 'bash -s'
+ssh -i ~/.ssh/droplet root@sprt.dev 'sudo systemctl restart sprt-dev.service'
+
+echo 'Shipped! 🚀'
