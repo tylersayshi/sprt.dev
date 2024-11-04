4a4c284cac4416e1e3621ad291ebb7cd30d36088
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-11-03 18:29:11 -0800

    switch script to use domain

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 GOOS=linux GOARCH=amd64 go build -o myapp
-sftp -i ~/.ssh/droplet root@147.182.244.225 <<< 'put myapp sprt-dev'
+sftp -i ~/.ssh/droplet root@sprt.dev <<< 'put myapp sprt-dev'
 
-echo "sudo systemctl restart sprt-dev.service && echo 'Done!'" | ssh -i ~/.ssh/droplet root@147.182.244.225 'bash -s'
+echo "sudo systemctl restart sprt-dev.service && echo 'Done!'" | ssh -i ~/.ssh/droplet root@sprt.dev 'bash -s'
