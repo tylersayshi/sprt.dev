226ff210c8854381570bcf62fa987556d76d3022
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-11-03 19:38:50 -0800

    remove download from deploy - seems like bad idea

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 GOOS=linux GOARCH=amd64 go build -o myapp
 sftp -i ~/.ssh/droplet root@sprt.dev <<< 'put myapp sprt-dev'
 
-echo "sudo systemctl restart sprt-dev.service && aws s3 cp s3://tylers-big-bucket/sprt-dev.db /root/sprt-dev.db && echo 'Done!'" | ssh -i ~/.ssh/droplet root@sprt.dev 'bash -s'
+echo "sudo systemctl restart sprt-dev.service && echo 'Done!'" | ssh -i ~/.ssh/droplet root@sprt.dev 'bash -s'
